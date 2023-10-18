e7a18441d13abef71215abde506e273c7e8b885b
Colby Klein <cwklein93@gmail.com>
2023-10-18 21:25:43 +0000

    setup

---
+++ app.py
@@ -0,0 +1,18 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+    <select name="cars">
+
+    <option value="volvo">Volvo</option>
+    <option value="saab">Saab</option>
+    <option value="mercedes">Mercedes</option>
+    <option value="audi">Audi</option>
+
+    </select>
